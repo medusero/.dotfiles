200f10a6ac51795f8398152cfe4291ec76a00f5a
medusero <github.guaranteer@slmail.me>
2023-03-14 00:02:05 +0100

    Cambios en los atajos de teclado para redimensionar los buffers

--- nvim/.config/nvim/lua/user/keymaps.lua
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -22,11 +22,11 @@ keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
--- Resize with arrows
-keymap("n", "<C-Up>", ":resize -2<CR>", opts)
-keymap("n", "<C-Down>", ":resize +2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+-- Resize buffers
+keymap("n", "<M-k>", ":resize -2<CR>", opts)
+keymap("n", "<M-j>", ":resize +2<CR>", opts)
+keymap("n", "<M-h>", ":vertical resize -2<CR>", opts)
+keymap("n", "<M-l>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
 keymap("n", "<S-l>", ":bnext<CR>", opts)
